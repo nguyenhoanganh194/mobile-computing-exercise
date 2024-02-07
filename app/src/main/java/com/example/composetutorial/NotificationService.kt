e7b9a341f075b780a93be8cf3a9f376b3e6b370a
Anh Nguyen <nguyenhoanganha1y@gmail.com>
2024-02-07 16:11:19 +0700

    Update NotificationService.kt

--- app/src/main/java/com/example/composetutorial/NotificationService.kt
+++ app/src/main/java/com/example/composetutorial/NotificationService.kt
@@ -7,6 +7,10 @@ import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Build
 import android.os.IBinder
 import android.util.Log
@@ -15,19 +19,92 @@ import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.example.composetutorial.R
 
-class NotificationService : Service() {
-
+class NotificationService : Service(), SensorEventListener {
+    private lateinit var sensorManager: SensorManager
+    private var accelerometer: Sensor? = null
     companion object {
         private const val NOTIFICATION_ID = 1
         private const val CHANNEL_ID = "MyChannel"
     }
 
+
+    override fun onCreate() {
+        super.onCreate()
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        accelerometer?.let {
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         // Create and display the notification
         createNotificationChannel()
-        showNotification()
+        showNotificationEnableApplication()
+
+
         return START_NOT_STICKY
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        sensorManager.unregisterListener(this)
+    }
+    override fun onSensorChanged(event: SensorEvent?) {
+        event?.let {
+            if (it.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+                val x = event.values[0]
+                val y = event.values[1]
+                val z = event.values[2]
+
+                val gForce = Math.sqrt((x * x + y * y + z * z).toDouble())
+
+                if (gForce > 1.2 * 10) { // Adjust this threshold as needed
+                    showSensorNotification(gForce)
+                }
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
+    private fun showSensorNotification(gForce : Double) {
+        try {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            val pendingIntent =
+                PendingIntent.getActivity(this, 0, intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+
+            val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Sensor notification " + gForce.toString())
+                .setContentText("Click here to open the application")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+
+
+            // Show the notification
+            val notificationManager = NotificationManagerCompat.from(this)
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+
+                Log.d("Test", "No pemission yet.")
+                return
+            }
+            Log.d("Test1", "Send intent message")
+            notificationManager.notify(NOTIFICATION_ID, builder.build())
+        }
+        catch (e : Exception){
+            e.printStackTrace()
+            Log.d("Test1",e.message.toString())
+        }
+    }
+
 
     private fun createNotificationChannel() {
         Log.d("Test1", "createNotificationChannel")
@@ -48,41 +125,41 @@ class NotificationService : Service() {
         }
     }
 
-    private fun showNotification() {
-        Log.d("Test1", "showNotification")
-//        val intent = Intent(this, MainActivity::class.java)
-//        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
-//        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+    private fun showNotificationEnableApplication() {
+        try {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            val pendingIntent =
+                PendingIntent.getActivity(this, 0, intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
 
-        // Build your notification using NotificationCompat.Builder
-        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_launcher_background)
-            .setContentTitle("Notification Title")
-            .setContentText("Notification Content")
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            //.setContentIntent(pendingIntent)
+            val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Enable notification")
+                .setContentText("Click here to open the application")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
 
 
-        // Show the notification
-        val notificationManager = NotificationManagerCompat.from(this)
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.POST_NOTIFICATIONS
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
+            // Show the notification
+            val notificationManager = NotificationManagerCompat.from(this)
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
 
-            Log.d("Test", "No pemission yet.")
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
+                Log.d("Test", "No pemission yet.")
+                return
+            }
+            Log.d("Test1", "Send intent message")
+            notificationManager.notify(NOTIFICATION_ID, builder.build())
         }
-        Log.d("Test1", "Send intent message")
-        notificationManager.notify(NOTIFICATION_ID, builder.build())
+        catch (e : Exception){
+            e.printStackTrace()
+            Log.d("Test1",e.message.toString())
+        }
+
     }
 
     override fun onBind(intent: Intent): IBinder? {
